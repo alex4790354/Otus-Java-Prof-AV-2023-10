813f3068350f1e3830cf0a4e616fa59f11e6ff74
Alexander V. <9720560@gmail.com>
2024-05-05 15:39:58 +0300

    hw32-concurrentCollections pre-hw commit

--- hw32-concurrentCollections/build.gradle.kts
+++ hw32-concurrentCollections/build.gradle.kts
@@ -1,6 +1,13 @@
 dependencies {
+    implementation ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok")
+
     implementation("ch.qos.logback:logback-classic")
-    implementation("org.flywaydb:flyway-core")
-    implementation("com.zaxxer:HikariCP")
-    implementation("org.postgresql:postgresql")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
 }
